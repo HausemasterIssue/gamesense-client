e60ebe2f3b24e1cdcb0ca07b2eb9a9fedebc714b
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-09-28 14:20:59 -0400

    dosent even work

--- src/main/java/com/gamesense/client/module/modules/misc/MultiTask.java
+++
@@ -1,9 +0,0 @@
-package com.gamesense.client.module.modules.misc;
-
-import com.gamesense.client.module.Category;
-import com.gamesense.client.module.Module;
-
-@Module.Declaration(name = "MultiTask", category = Category.Misc)
-public class MultiTask extends Module {
-
-}
