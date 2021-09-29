bf873deacaefa3019908a3d2e0ce1ae8fcb7656d
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-09-29 11:21:10 -0400

    2b2t bypass criticals

---
+++ src/main/java/com/gamesense/client/module/modules/combat/Criticals.java
@@ -0,0 +1,56 @@
+package com.gamesense.client.module.modules.combat;
+
+import java.util.Arrays;
+import com.gamesense.api.event.events.PacketEvent;
+import com.gamesense.api.setting.values.ModeSetting;
+import com.gamesense.client.module.Category;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import com.gamesense.client.module.Module;
+import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.network.play.client.CPacketUseEntity;
+
+/*
+ * @author hausemasterissue
+ * @since 9/29/2021
+ */
+
+@Module.Declaration(name = "Criticals", category = Category.Combat)
+public class Criticals extends Module {
+	
+	ModeSetting critMode = registerMode("Mode", Arrays.asList("Packet", "Jump", "NCPStrict"), "NCPStrict");
+	
+    @EventHandler
+    private final Listener<PacketEvent.Send> listener = new Listener<>(event -> {
+        if (event.getPacket() instanceof CPacketUseEntity) {
+            if (((CPacketUseEntity) event.getPacket()).getAction() == CPacketUseEntity.Action.ATTACK && mc.player.onGround) {
+            	switch (critMode.getValue()) {
+                case "Jump": {
+                    mc.player.jump();
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY - 0.05, mc.player.posZ, false));
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
+                    break;
+                }
+
+                case "Packet": {
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.3, mc.player.posZ, false));
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
+                    break;
+                }
+
+                case "NCPStrict": {
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.062602401692772D, mc.player.posZ, false));
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.0726023996066094D, mc.player.posZ, false));
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
+                    break;
+                }
+            }
+        }
+            }
+        }
+    );
+	
+    }
+
+
