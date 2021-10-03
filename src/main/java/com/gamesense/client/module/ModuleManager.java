b36567fcb9ae3e88c044219f2a95fe09fc2ca818
christallinqq <66845682+christallinqq@users.noreply.github.com>
2021-10-03 18:22:44 -0400

    Update ModuleManager.java

--- src/main/java/com/gamesense/client/module/ModuleManager.java
+++ src/main/java/com/gamesense/client/module/ModuleManager.java
@@ -50,7 +50,6 @@ public class ModuleManager {
         addMod(new NoSwing());
         addMod(new Reach());
         addMod(new PacketUse());
-        addMod(new AutoDupe());
         addMod(new PacketXP());
         addMod(new PortalGodmode());
         //Movement
