fafbf76c504aa49a1a9fd359fc1ed6cbdff82b57
Sxmurai <gavrw0607@gmail.com>
2021-10-04 09:09:58 -0500

    [cleaup]: cleanup criticals code

--- src/main/java/com/gamesense/client/module/modules/combat/Criticals.java
+++ src/main/java/com/gamesense/client/module/modules/combat/Criticals.java
@@ -1,20 +1,17 @@
 package com.gamesense.client.module.modules.combat;
 
-import java.util.Arrays;
-
 import com.gamesense.api.event.events.PacketEvent;
 import com.gamesense.api.setting.values.ModeSetting;
 import com.gamesense.client.module.Category;
-import me.zero.alpine.listener.EventHandler;
-import me.zero.alpine.listener.Listener;
 import com.gamesense.client.module.Module;
 import com.mojang.realmsclient.gui.ChatFormatting;
-import net.minecraft.client.Minecraft;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.client.CPacketUseEntity;
-import net.minecraft.world.World;
 
+import java.util.Arrays;
 
 /*
  * @author hausemasterissue
@@ -27,16 +24,15 @@ public class Criticals extends Module {
 	
 	ModeSetting critMode = registerMode("Mode", Arrays.asList("Packet", "Jump", "NCPStrict"), "NCPStrict");
 
-	CPacketUseEntity packet;
-	final Minecraft mc = Minecraft.getMinecraft();
-
     @EventHandler
-    private final Listener<PacketEvent.Send> listener = new Listener<>(event -> {
+    private final Listener<PacketEvent.Send> packetSendListener = new Listener<>(event -> {
         if (PacketEvent.getPacket() instanceof CPacketUseEntity) {
-        	packet = (CPacketUseEntity)PacketEvent.getPacket();
-            if (((CPacketUseEntity) PacketEvent.getPacket()).getAction() == CPacketUseEntity.Action.ATTACK && packet.getEntityFromWorld((World)this.mc.world) instanceof EntityLivingBase &&
-            		mc.player.onGround && !mc.player.isInLava() && !mc.player.isInWater()) {
-            	switch (critMode.getValue()) {
+            CPacketUseEntity packet = (CPacketUseEntity) PacketEvent.getPacket();
+            if (packet.getAction() != CPacketUseEntity.Action.ATTACK || !(packet.getEntityFromWorld(mc.world) instanceof EntityLivingBase) || !mc.player.onGround || mc.player.isInLava() || mc.player.isInWater()) {
+                return;
+            }
+
+            switch (critMode.getValue()) {
                 case "Jump": {
                     mc.player.jump();
                     mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY - 0.05, mc.player.posZ, false));
@@ -59,24 +55,11 @@ public class Criticals extends Module {
                 }
             }
         }
-            }
-        }
-    );
+    });
     
     public String getHudInfo() {
-        String t = "";
-        if (critMode.getValue().equalsIgnoreCase("Packet")){
-            t = "[" + ChatFormatting.WHITE + critMode.getValue() + ChatFormatting.GRAY + "]";
-        } else if (critMode.getValue().equalsIgnoreCase("Jump")) {
-        	t = "[" + ChatFormatting.WHITE + critMode.getValue() + ChatFormatting.GRAY + "]";
-        } else if (critMode.getValue().equalsIgnoreCase("NCPStrict")) {
-        	t = "[" + ChatFormatting.WHITE + critMode.getValue() + ChatFormatting.GRAY + "]";
-        }
-
-        return t;
+        return "[" + ChatFormatting.WHITE + this.critMode.getValue() + ChatFormatting.GRAY + "]";
     }
-    
-	
 }
 
 
