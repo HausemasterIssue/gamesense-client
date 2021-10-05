373887c64e90500ebccabe99ec3b304b828efbf7
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-10-05 17:07:19 -0400

    lol im dumb

--- src/main/java/com/gamesense/client/module/modules/movement/IceSpeed.java
+++ src/main/java/com/gamesense/client/module/modules/movement/IceSpeed.java
@@ -7,11 +7,7 @@ import net.minecraft.init.Blocks;
 
 @Module.Declaration(name = "IceSpeed", category = Category.Movement)
 public class IceSpeed extends Module {
-	
-    DoubleSetting iceSpeed = registerDouble("IceSpeed", 0.4, 0.0, 5.0);
-    DoubleSetting packedSpeed = registerDouble("PackedSpeed", 0.4, 0.0, 5.0);
-	DoubleSetting iceSpeed = registerDouble("IceSpeed", 0.4, 0.0, 5.0);
-	
+		
     public void onDisable() {
         Blocks.ICE.setDefaultSlipperiness(0.98f);
         Blocks.PACKED_ICE.setDefaultSlipperiness(0.98f);
