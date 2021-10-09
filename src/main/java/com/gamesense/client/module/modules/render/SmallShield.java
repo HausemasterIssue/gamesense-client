b5c4739dbed17556eafd7d5e55cd7721016e027d
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-10-09 16:07:40 -0400

    reverting my autism

--- src/main/java/com/gamesense/client/module/modules/render/SmallShield.java
+++ src/main/java/com/gamesense/client/module/modules/render/SmallShield.java
@@ -3,7 +3,6 @@ package com.gamesense.client.module.modules.render;
 import com.gamesense.api.setting.values.DoubleSetting;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
-
 import net.minecraft.client.renderer.ItemRenderer;
 
 /*
@@ -14,14 +13,12 @@ import net.minecraft.client.renderer.ItemRenderer;
 @Module.Declaration(name = "SmallShield", category = Category.Render)
 public class SmallShield extends Module {
 	
-	DoubleSetting heightOffhand = registerDouble("OffhandHeight", 0.5, 0.1, 1.0);
-	DoubleSetting heightMainhand = registerDouble("MainhandHeight", 1.0, 0.1, 1.0);
+	DoubleSetting height = registerDouble("Height", 0.5, 0.1, 1.0);
 	
 	ItemRenderer itemRenderer = mc.entityRenderer.itemRenderer;
 	
 	public void onUpdate() {
-        itemRenderer.equippedProgressOffHand = heightOffhand.getValue().floatValue();
-        itemRenderer.equippedProgressMainHand = heightMainhand.getValue().floatValue();
+        	itemRenderer.equippedProgressOffHand = height.getValue().floatValue();
 	}
 
 
