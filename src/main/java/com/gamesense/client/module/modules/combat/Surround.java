44bc36108521f8141c3a9ca9294c43da04b3ac41
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-10-20 14:15:20 -0400

    silent swap fixes

--- src/main/java/com/gamesense/client/module/modules/combat/Surround.java
+++ src/main/java/com/gamesense/client/module/modules/combat/Surround.java
@@ -19,12 +19,12 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
-import net.minecraft.network.play.client.CPacketEntityAction;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-
+import net.minecraft.network.play.client.CPacketEntityAction;
+import net.minecraft.network.play.client.CPacketHeldItemChange;
 import java.util.Arrays;
 
 /**
