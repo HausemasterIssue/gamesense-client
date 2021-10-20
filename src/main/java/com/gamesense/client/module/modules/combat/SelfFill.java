24d76b5f886b1240bf79af487c3cdc222107820b
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-10-20 13:43:14 -0400

    reverting burrow to a worse version (according to sxmurai)

--- src/main/java/com/gamesense/client/module/modules/combat/SelfFill.java
+++ src/main/java/com/gamesense/client/module/modules/combat/SelfFill.java
@@ -1,96 +1,214 @@
-package com.gamesense.client.module.modules.combat;
+package com.gamesense.client.module.modules.exploits;
 
-import com.gamesense.api.setting.values.BooleanSetting;
-import com.gamesense.api.setting.values.DoubleSetting;
-import com.gamesense.api.setting.values.ModeSetting;
-import com.gamesense.api.util.player.InventoryUtil;
-import com.gamesense.api.util.player.PlacementUtil;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
-import net.minecraft.block.Block;
+import com.gamesense.api.setting.values.BooleanSetting;
+import com.gamesense.api.setting.values.IntegerSetting;
+import com.gamesense.api.util.misc.MessageBus;
+import net.minecraft.block.BlockEnderChest;
+import net.minecraft.block.BlockObsidian;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
+import net.minecraft.network.play.client.CPacketEntityAction;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-
-import java.util.Arrays;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.client.CPacketHeldItemChange;
+import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
-* @author Sxmurai
-* @since 11/10/2021
-* previously "made" by Hause, but the code made me wanna commit not live so
-* taken from my client, Inferno. (instant burrow mode)
-* origin of code: https://github.com/ciruu1/InstantBurrow
+* pasted from legacy, rewritten by sxmurai, reverted by hause
+* just reverting this because
+* sxmurai's dosent work at all lol
+* burrow is still chinese idc anymore
 */
+
 @Module.Declaration(name = "SelfFill", category = Category.Combat)
 public class SelfFill extends Module {
-    DoubleSetting rubberband = registerDouble("Rubberband", 3.0, -5.0, 5.0);
-    BooleanSetting rotate = registerBoolean("Rotate", true);
-    BooleanSetting swing = registerBoolean("Swing", true);
-    ModeSetting block = registerMode("Block", Arrays.asList("Obsidian", "EChest", "EndRod"), "Obsidian");
-    BooleanSetting silent = registerBoolean("SilentSwitch", false);
-
-    private BlockPos origin = null;
-    private EnumHand hand = EnumHand.MAIN_HAND;
+	
+	IntegerSetting offset = registerInteger("Offset", 3, -5, 5);
+	BooleanSetting rotate = registerBoolean("Rotate", true);
+	BooleanSetting echest = registerBoolean("EChest", true);
+	
+	private BlockPos originalPos;
     private int oldSlot = -1;
 
     @Override
-    protected void onEnable() {
-        if (mc.world == null || mc.player == null) {
-            this.toggle();
+    public void onEnable() {
+        super.onEnable();
+
+        originalPos = new BlockPos(mc.player.posX, mc.player.posY, mc.player.posZ);
+
+        if (mc.world.getBlockState(new BlockPos(mc.player.posX, mc.player.posY, mc.player.posZ)).getBlock().equals(Blocks.OBSIDIAN) ||
+                intersectsWithEntity(this.originalPos)) {
+            toggle();
             return;
         }
 
-        this.origin = new BlockPos(mc.player.posX, mc.player.posY, mc.player.posZ);
-        if (!mc.world.isAirBlock(this.origin.add(0.0, 1.0, 0.0)) || !mc.world.isAirBlock(this.origin.add(0.0, 2.0, 0.0))) {
-            this.toggle();
-            return;
-        }
-
-        Block block = this.block.getValue().equals("Obsidian") ? Blocks.OBSIDIAN : this.block.getValue().equals("EChest") ? Blocks.ENDER_CHEST : Blocks.END_ROD;
-        int slot = InventoryUtil.getHotbarBlockSlot(block, true);
-
-        if (slot == -1) {
-            this.toggle();
-            return;
-        }
-
-        this.hand = slot == 45 ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND;
-        if (this.hand == EnumHand.MAIN_HAND) {
-            this.oldSlot = mc.player.inventory.currentItem;
-            InventoryUtil.switchTo(slot, this.silent.getValue());
-        }
+        oldSlot = mc.player.inventory.currentItem;
     }
 
     @Override
-    protected void onDisable() {
-        this.origin = null;
-        this.hand = EnumHand.MAIN_HAND;
-
-        if (mc.world != null && mc.player != null && this.oldSlot != -1) {
-            InventoryUtil.switchTo(this.oldSlot, this.silent.getValue());
-        }
-
-        this.oldSlot = -1;
-    }
-
-    @Override
-    public void onUpdate() {
-        if (mc.world == null || mc.player == null) {
+    public void onUpdate()
+    {
+        if (echest.getValue() ? findHotbarBlock(BlockEnderChest.class) == -1  : findHotbarBlock(BlockObsidian.class) == -1) {
+            MessageBus.sendClientPrefixMessage(echest.getValue() ? "You have no EChests in your hotbar!" : "You have no obisidan in your hotbar!");
+            toggle();
             return;
         }
 
-        if (this.origin != null) {
-            mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.41999998688698, mc.player.posZ, true));
-            mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.7531999805211997, mc.player.posZ, true));
-            mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.00133597911214, mc.player.posZ, true));
-            mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.16610926093821, mc.player.posZ, true));
+        switchToSlot(echest.getValue() ? findHotbarBlock(BlockEnderChest.class) : findHotbarBlock(BlockObsidian.class));
 
-            PlacementUtil.place(this.origin, this.hand, this.rotate.getValue(), this.swing.getValue(), true);
+        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.41999998688698D, mc.player.posZ, true));
+        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.7531999805211997D, mc.player.posZ, true));
+        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.00133597911214D, mc.player.posZ, true));
+        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 1.16610926093821D, mc.player.posZ, true));
 
-            mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + this.rubberband.getValue(), mc.player.posZ, true));
+        placeBlock(originalPos, EnumHand.MAIN_HAND, rotate.getValue(), true, false);
 
-            this.toggle();
-        }
+        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + offset.getValue(), mc.player.posZ, false));
+
+        mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.STOP_SNEAKING));
+        mc.player.setSneaking(false);
+
+        switchToSlot(oldSlot);
+
+        toggle();
     }
+
+    private boolean intersectsWithEntity(final BlockPos pos) {
+        for (final Entity entity : mc.world.loadedEntityList) {
+            if (entity.equals(mc.player)) continue;
+            if (entity instanceof EntityItem) continue;
+            if (new AxisAlignedBB(pos).intersects(entity.getEntityBoundingBox())) return true;
+        }
+        return false;
+    }
+    
+    public static boolean placeBlock(BlockPos pos, EnumHand hand, boolean rotate, boolean packet, boolean isSneaking) {
+        boolean sneaking = false;
+        EnumFacing side = getFirstFacing(pos);
+        if (side == null) {
+            return isSneaking;
+        }
+
+        BlockPos neighbour = pos.offset(side);
+        EnumFacing opposite = side.getOpposite();
+
+        Vec3d hitVec = new Vec3d(neighbour).add(0.5, 0.5, 0.5).add(new Vec3d(opposite.getDirectionVec()).scale(0.5));
+        Block neighbourBlock = mc.world.getBlockState(neighbour).getBlock();
+
+        if (!mc.player.isSneaking()) {
+            mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.START_SNEAKING));
+            mc.player.setSneaking(true);
+            sneaking = true;
+        }
+
+        if (rotate) {
+            faceVector(hitVec, true);
+        }
+
+        rightClickBlock(neighbour, hitVec, hand, opposite, packet);
+        mc.player.swingArm(EnumHand.MAIN_HAND);
+        mc.rightClickDelayTimer = 4; //?
+        return sneaking || isSneaking;
+    }
+    
+    public static List<EnumFacing> getPossibleSides(BlockPos pos) {
+        List<EnumFacing> facings = new ArrayList<>();
+        for (EnumFacing side : EnumFacing.values()) {
+            BlockPos neighbour = pos.offset(side);
+            if (mc.world.getBlockState(neighbour).getBlock().canCollideCheck(mc.world.getBlockState(neighbour), false)) {
+                IBlockState blockState = mc.world.getBlockState(neighbour);
+                if (!blockState.getMaterial().isReplaceable()) {
+                    facings.add(side);
+                }
+            }
+        }
+        return facings;
+    }
+    
+    public static EnumFacing getFirstFacing(BlockPos pos) {
+        for (EnumFacing facing : getPossibleSides(pos)) {
+            return facing;
+        }
+        return null;
+    }
+
+    public static Vec3d getEyesPos() {
+        return new Vec3d(mc.player.posX, mc.player.posY + mc.player.getEyeHeight(), mc.player.posZ);
+    }
+    
+    public static float[] getLegitRotations(Vec3d vec) {
+        Vec3d eyesPos = getEyesPos();
+        double diffX = vec.x - eyesPos.x;
+        double diffY = vec.y - eyesPos.y;
+        double diffZ = vec.z - eyesPos.z;
+        double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
+
+        float yaw = (float) Math.toDegrees(Math.atan2(diffZ, diffX)) - 90F;
+        float pitch = (float) -Math.toDegrees(Math.atan2(diffY, diffXZ));
+
+        return new float[]{
+                mc.player.rotationYaw + MathHelper.wrapDegrees(yaw - mc.player.rotationYaw),
+                mc.player.rotationPitch + MathHelper.wrapDegrees(pitch - mc.player.rotationPitch)
+        };
+    }
+
+    public static void faceVector(Vec3d vec, boolean normalizeAngle) {
+        float[] rotations = getLegitRotations(vec);
+        mc.player.connection.sendPacket(new CPacketPlayer.Rotation(rotations[0], normalizeAngle ? MathHelper.normalizeAngle((int) rotations[1], 360) : rotations[1], mc.player.onGround));
+    }
+
+    public static void rightClickBlock(BlockPos pos, Vec3d vec, EnumHand hand, EnumFacing direction, boolean packet) {
+        if (packet) {
+            float f = (float) (vec.x - (double) pos.getX());
+            float f1 = (float) (vec.y - (double) pos.getY());
+            float f2 = (float) (vec.z - (double) pos.getZ());
+            mc.player.connection.sendPacket(new CPacketPlayerTryUseItemOnBlock(pos, direction, hand, f, f1, f2));
+        } else {
+            mc.playerController.processRightClickBlock(mc.player, mc.world, pos, direction, vec, hand);
+        }
+        mc.player.swingArm(EnumHand.MAIN_HAND);
+        mc.rightClickDelayTimer = 4;
+    }
+
+    public static int findHotbarBlock(Class clazz) {
+        for (int i = 0; i < 9; i++) {
+            ItemStack stack = mc.player.inventory.getStackInSlot(i);
+            if (stack == ItemStack.EMPTY) {
+                continue;
+            }
+
+            if (clazz.isInstance(stack.getItem())) {
+                return i;
+            }
+
+            if (stack.getItem() instanceof ItemBlock) {
+                Block block = ((ItemBlock) stack.getItem()).getBlock();
+                if (clazz.isInstance(block)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void switchToSlot(final int slot) {
+        mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
+        mc.player.inventory.currentItem = slot;
+        mc.playerController.updateController();
+    }
+
 }
