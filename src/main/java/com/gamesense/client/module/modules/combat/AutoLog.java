322cf6e8459f8056724fea1f3302ae0d64b082cb
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-10-20 16:50:55 -0400

    autolog fixed

--- src/main/java/com/gamesense/client/module/modules/combat/AutoLog.java
+++ src/main/java/com/gamesense/client/module/modules/combat/AutoLog.java
@@ -1,34 +1,43 @@
 package com.gamesense.client.module.modules.combat;
 
-import com.gamesense.client.module.Module;
-import net.minecraft.init.Items;
-import net.minecraft.util.text.TextComponentString;
+import com.gamesense.api.setting.values.BooleanSetting;
 import com.gamesense.api.setting.values.IntegerSetting;
 import com.gamesense.api.util.player.InventoryUtil;
 import com.gamesense.client.module.Category;
+import com.gamesense.client.module.Module;
+import net.minecraft.init.Items;
+import net.minecraft.util.text.TextComponentString;
 
 /*
 * @author hausemasterissue
-* @since 6/10/2021
-* credits momentum, cookie client
+* @since 20/10/2021
+* credits to momentum, cookie client
 */
 
 @Module.Declaration(name = "AutoLog", category = Category.Combat)
 public class AutoLog extends Module {
 	
-	IntegerSetting health = registerInteger("Health", 0, 0, 36);
-	IntegerSetting noTotems = registerInteger("Totems", 0, 0, 36);
+	BooleanSetting healthLog = registerBoolean("HealthLog", false);
+	IntegerSetting health = registerInteger("Health", 4, 0, 36);
+	BooleanSetting totemLog = registerBoolean("TotemLog", true);
+	IntegerSetting noTotems = registerInteger("Totems", 1, 0, 36);
 	
 	@Override
     public void onUpdate() {
         if (mc.world == null || mc.player == null)
             return;
-
-        if (mc.player.getHealth() == health.getValue())
-        	log("[SpiderSense] [AutoLog] You have been logged out do to being lower than the minimum health allowed");
-
-        if (InventoryUtil.getItemCount(Items.TOTEM_OF_UNDYING) == noTotems.getValue())
-        	log("[SpiderSense] [AutoLog] You have been logged out do to exceeding the minimum amount of Totems");
+	    
+	if(healthLog.getValue()) {
+		if(mc.player.getHealth() <= health.getvalue()) {
+			log("[SpiderSense] [AutoLog] You have been logged out do to being lower than the minimum health allowed");
+		}
+	}
+	    
+	if(totemLog.getValue()) {
+		if(InventoryUtil.getItemCount(Items.TOTEM_OF_UNDYING) <= noTotems.getValue()) {
+			log("[SpiderSense] [AutoLog] You have been logged out do to having the minimum amount of totems allowed");
+		}
+	}
 
     }
 	
