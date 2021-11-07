48a2a668508c12e82299bf1870fecb0150bdbc59
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-11-07 15:32:55 -0500

    sniff paint

--- src/main/java/com/gamesense/client/module/modules/misc/Spammer.java
+++ src/main/java/com/gamesense/client/module/modules/misc/Spammer.java
@@ -18,10 +18,11 @@ import java.util.Random;
 @Module.Declaration(name = "Spammer", category = Category.Misc)
 public class Spammer extends Module {
   
-  ModeSetting mode = registerMode("Mode", Arrays.asList("Toxic", "Advertise", "AntiRacist", "Clients"), "Toxic");
+  ModeSetting mode = registerMode("Mode", Arrays.asList("Toxic", "Advertise", "AntiRacist", "Clients", "JaredVPopBob", "Motivation"), "Toxic");
   IntegerSetting delaySeconds = registerInteger("Delay", 5, 0, 180);
   
   private int delay = 0;
+  private int funnePlace = -1;
   private String[] mToxic = {"the 11 year old is getting swatted in vc LOL!", "my richness powered by spidersense", 
                             "compare funds rn LOL!", "getting ur ip rn, gimme 1 sec", "pooron roflsauce", 
                             "im the KING rofl", "im richer than your entire bloodline nn", "SO POOR LOOOL",
@@ -30,7 +31,10 @@ public class Spammer extends Module {
                             "nobodys talking cuz they know thats a SWAT LMAOO", "speak up nn, i couldnt hear your nn talk", 
                             "POP MORE ROFLLL", "i have your dox LOL", "shut up before you get a SWAT LOOOLOLOL",
                             "guys hes bri'ish LMFAOOO", "look out bro the ii goon squad is at your window!!!",
-                            "spidersense owns me and all LOL", "fundless faggot LEL!"};
+                            "spidersense owns me and all LOL", "fundless faggot LEL!", "i told the gang your adress nigga",
+                            "GANG GANG", "my dogs wil rip you into pieces LOOOOOL", "bro your so fucking fat LELELEL!!!",
+                            "im more famous than your all your ancestors COMBINED!", "your iq is lower than room temp lol",
+                            "check the bin nigga"};
   private String[] mAd = {"SpiderSense owns me and all!", "The CA in SpiderSense is too fast bro!",
                                      "HausemasterIssue and his shitty client are so based!", "OMFG HOW IS SPIDERSENSE THIS GOOD???", 
                                      "SpiderSense makes me 500% better at CPVP!", "I just gained like, 3 000 000 funds from SpiderSense!", 
@@ -59,6 +63,35 @@ public class Spammer extends Module {
                                    "Still using Inertia? Wow, that's sad.", "Nutgod.cc is so good", "LeuxBackdoor? You are a literal fucking monkey",
                                    "Hyperlethal is just Wurst+2", "Impact is a RAT I repeat IMPACT is a RAT!", "Meteor? Take a toaster bath please.",
                                    "SPIDERSENSE IS AT THE TIPPITY TOP OF ALL CLIENTS!"};
+  // lol yes i legitmatley listenened to the video and wrote down all the lines
+  private String[] mFun = {"Alright, the following logs are from December 25 2015 on 2b2t", "I'm going to attempt to recapture the magic of this moment",
+                          "Now there is over 900 lines of chat", "But I've condesned it down a little shorter", "It starts out with popbob saying \"I'm out of alchohol\"",
+                          "To which Jared responds, \"Pop, you can drink my semen\"", "Popbob reponds, \"Shut the fuck up\"", "Jared using green text \"Driving your 20 year old car, can't afford a new one because booze money\"",
+                          "Popbob exlaims that Jared is triggered", "To which jared responds, \"Pop, your obiese\"", "To which pop responds \"You fat FUCK!\"", 
+                          "Jared exlaims \"Because your parents drive used shit from 1970\"", "Popbob once again, \"Lose some weight\"", 
+                          "Jared responds, \"Pop, what's it like being 500 pounds over the national average?\"", "Popbob says \"Stay mad Jared Miller\"",
+                          "Jared says \"Stay mad Alan Schafer\"", "Popbob responds \"Yeah your name is miller because your parents were lamen, fucking loser!",
+                          "Your so fat your ancesters are losers too LMAO!\"", "No this continues for a few more minutes, but climaxes with Jared exlaiming: ",
+                          "\"Go sniff paint you braindamaged kike\"", "Popbob responds, \"I will fuck your ass fag\"", "Jared says, \"Not if I fuck yours first!\"",
+                          "To which popbob responds, \"Your too weak willed and feminine for that\"", "I want you to let that sink in for a moment", 
+                           "Now the rest of it, if you want to read through it go right ahead", "But I've just condensed the more colorful quotes for you"
+                           "\"Go play in traffic\"", "\"Your family should abanden you\"", "\"Go pray to God faggot\"", "\"Pop, your the result of countless generations of inbreeding\"", 
+                           "\"Your more autsitic that Chris Chan\"", "\"He's going to autism himself into an autism white drwarf, he will then eventually collapse into an assburgers blackhole\"",
+                           "\"Pop your too scared to even fight me at IMPS\"", "\"Everyone knows about your fetish for cross dressing Jared\"", 
+                           "\"Jared is 16 years old, he's still in high school, born in 1999, he is fat. as. fuck.\"", "Now if you would like to read the entirity of this exchange I will leave a link below."};
+  public String[] mMotiv = {"Others will stop playing after a few minutes, we will play until we are known!", "Normal people get mad, we get happy!",
+                           "Randoms will stay male, we will become trans!", "Others will be humble, we will have huge egos!", 
+                           "Many quit because of the queue, but we live for the queue!", "Normal people will beg, we earn.",
+                           "Noname's will judge on personality, we will judge on namemc views!", "Most get mad at hackers, we praise hackers!",
+                           "Others will die once and say it was lag, we will die and say it was desync!", "Normal people will cry once they hear us, we will gleam in joy!",
+                           "Many will quit after losing all their gear, but we use it to steal more gear!", "Noname's will stay mad, we will stay joyful",
+                           "Others will practice for an hour a day, we practice for a month a day!", "To others, it's a game, to us it is our existence!",
+                           "Many will report after being called the n-word, we will give them gifts for saying it!", "Randoms will leave after dying in a second, we keep playing.",
+                           "Others think swearing is bad, we think of it as a great thing!", "Many will think hacking is bad, but to us, it is a way of life.",
+                           "Non's see popbob and scream, we see popbob and cum!", "Noname's will respect women, we will fuck them!",
+                           "Many will cry after playing for hours, we will find that persons dox!", "Others have little kid voices, we have female voices!",
+                           "Many shit themselves after someone knows their adress, we welcome them.", "Randoms will think Future is good, we will think SpiderSense is great!",
+                           "Others will like democracy, we will love communism!"};
 
   public void onUpdate() {
     Random rToxic = new Random();
@@ -69,16 +102,28 @@ public class Spammer extends Module {
     int randomRacist = rRacist.nextInt(mRacist.length);
     Random rClients = new Random();
     int randomClients = rClients.nextInt(mClients.length);
+    Random rMotiv = new Random();
+    int randomMotiv = rMotiv.nextInt(mMotiv.length);
     delay++;
+    funnePlace++;
     if (delay > (Integer) delaySeconds.getValue() * 20) {
         if(mode.getValue().equalsIgnoreCase("Toxic")) {
             mc.player.sendChatMessage(mToxic[randomToxic]);
+            funnePlace = 0;  
         } else if (mode.getValue().equalsIgnoreCase("Advertise")) {
             mc.player.sendChatMessage(mAd[randomAd]);
+            funnePlace = 0;
         } else if (mode.getValue().equalsIgnoreCase("AntiRacist")) {
             mc.player.sendChatMessage(mRacist[randomRacist]);
+            funnePlace = 0;
         } else if (mode.getValue().equalsIgnoreCase("Clients")) {
             mc.player.sendChatMessage(mClients[randomClients]);
+            funnePlace = 0;
+        } else if (mode.getValue().equalsIgnoreCase("JaredVPopBob")) {
+            mc.player.sendChatMessage(mFun[funnePlace]); 
+        } else if (mode.getValue().equalsIgnoreCase("Motivation")) {
+            mc.player.sendChatMessage(mMotiv[randomMotiv]);
+            funnePlace = 0;
         }
       
         delay = 0;
