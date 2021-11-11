cdf9f7e0d86fb60351e06984b6441baa0498ad20
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-11-11 08:49:50 -0500

    (impl) improve inventory move

--- src/main/java/com/gamesense/client/module/modules/movement/InventoryMove.java
+++ src/main/java/com/gamesense/client/module/modules/movement/InventoryMove.java
@@ -3,13 +3,19 @@ package com.gamesense.client.module.modules.movement;
 import com.gamesense.client.module.Category;
 import com.gamesense.client.module.Module;
 import net.minecraft.client.gui.GuiChat;
+import net.minecraft.client.gui.inventory.GuiEditSign;
 import net.minecraft.client.settings.KeyBinding;
 import org.lwjgl.input.Keyboard;
+
+/*
+* @author hausemasterissue
+* @since 11/11/2021
+*/
 
 @Module.Declaration(name = "InventoryMove", category = Category.Movement)
 public class InventoryMove extends Module {
 	
-	private static final KeyBinding[] MOVEMENT_KEYS = new KeyBinding[] {
+	private static final KeyBinding[] moveKeys = new KeyBinding[] {
             mc.gameSettings.keyBindForward,
             mc.gameSettings.keyBindBack,
             mc.gameSettings.keyBindRight,
@@ -20,11 +26,13 @@ public class InventoryMove extends Module {
     };
 	
 	public void onUpdate() {
-		if (mc.currentScreen != null && !(mc.currentScreen instanceof GuiChat)) {
-            for (KeyBinding binding : InventoryMove.MOVEMENT_KEYS) {
-                KeyBinding.setKeyBindState(binding.getKeyCode(), Keyboard.isKeyDown(binding.getKeyCode()));
-            }
-        }
+		if (mc.currentScreen instanceof GuiChat || mc.currentScreen instanceof GuiEditSign || mc.currentScreen == null) {
+            		return;
+        	}
+		
+		for (KeyBinding bind : moveKeys) {
+             		KeyBinding.setKeyBindState(bind.getKeyCode(), Keyboard.isKeyDown(bind.getKeyCode()));
+        	}
 	}
 
 }
